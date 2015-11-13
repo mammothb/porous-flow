1c4dc44e313a89e6da87bfbf60aa7fa241bf9b12
mammothb <mammothb+git@gmail.com>
2015-11-13 11:48:33 +0800

    editted formula to achieve trend observed in paper

--- porous_flow.m
+++ porous_flow.m
@@ -1,26 +1,25 @@
 clear;
 clf;
-% Model parameters
-H = 80;  % Height of domain
+% Model parameters (fixed)
 porosity = 0.1;  % Porosity of porous medium
 Re = 0.01;  % Reynolds number
 Da = 1e-5;  % Darcy number
 J = 1.0;  % Viscosity ratio
 tau = 0.8;  % Relaxation time
-% dx = 0.0316;  % Lattice spacing
-% dt = 0.001;  % Time step
 dx = 1.0;  % Lattice spacing
 dt = 1.0;  % Time step
-nu = (0.8 - 0.5) * dt * dx * dx / dt / dt / 3.0;  % Kinematic viscosity
+cs_sqr = dx * dx / dt / dt / 3.0;
+nu = (tau - 0.5) * cs_sqr * dt;  % Kinematic viscosity
 nu_e = J * nu;  % Effective parameter
-G = 10.0;  % External force
+H = 80;  % Height of domain
+
+% Derived parameters
 K = Da * H * H;  % Permeability
 F_epsilon = 1.75 / sqrt(150 * porosity^3);  % Geometric function
+u0 = Re * nu / H;  % Peak velocity
 r = sqrt(nu * porosity / K / nu_e);
-u0 = G * K / nu * (1 - acosh(r * H / 2));
-% u0 = Re * nu / H / dx;
-% disp(u0);
-disp(acosh(r * H / 2));
+% Error in paper?
+G = u0 * nu / K / (1 - 1 / cosh(r * H / 2));
 
 % Simulation parameters
 h = 0.08;  % Space step
@@ -30,7 +29,7 @@ g0 = 0.0;  % BC at y = 0
 gH = 0.0;  % BC at y = H
 A = h * h * F_epsilon * porosity / nu_e / sqrt(K);  % Coefficient for u^2
 B = h * h * nu * porosity / K / nu_e;  % Coefficient for u
-C = h * h * G * porosity / nu_e;  % Source term
+C = h * h * porosity / nu_e;  % Source term
 
 y = linspace(0, H, n);  % Discretized domain
 u = zeros(1, n);  % Initialize u
@@ -45,11 +44,11 @@ u(end) = gH;  % Apply boundary conditions
 while is_unsteady
   iterations = iterations + 1;
   u_new(2 : end - 1) = (u(3 : end) + u(2 : end - 1) + u(1 : end - 2) - A .*...
-      u(2 : end - 1) .^ 2 - B .* u(2 : end - 1) + C) / 3.0;
+      u(2 : end - 1) .^ 2 - B .* u(2 : end - 1) + C * G) / 3.0;
   relative_err = max(abs(u_new - u));
   % fprintf('%d\n', relative_err);
   if relative_err < tolerance, is_unsteady = false; end
   u = u_new;
 end
 fprintf('Iterations: %d\n', iterations);
-plot (y, u);
+plot (y, u ./ u0);
